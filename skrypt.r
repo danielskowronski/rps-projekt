647163621b14e9581585ba688683f458288396be
Daniel Skowroński <daniel@dsinf.net>
2016-06-10 22:40:16 +0200

    report contribution, R: ip plots, top ip

--- skrypt.r
+++ skrypt.r
@@ -23,3 +23,14 @@ barplot(prop.table(table(dates_weekdays)))
 
 #agregacja oczekiwanych encoding types
 barplot(table(file_full$etypes))
+
+#badanie adresów IP
+ip<-table(unlist(file_full$ip))
+plot(ip)
+ip<-as.data.frame(ip)
+ip<-ip[ip$Freq>6*IQR(ip$Freq),] # ~=> ip00006, ip00008
+wysokie_ip<-subset(file_full, ip=="ip00006" | ip=="ip00008")
+dates <- as.Date(as.POSIXct(wysokie_ip$authtime, origin="1970-01-01"))
+datetimes <- as.POSIXct(wysokie_ip$authtime, origin="1970-01-01")
+dates_weekdays <- format(datetimes, "%u")
+barplot(prop.table(table(dates_weekdays)))
