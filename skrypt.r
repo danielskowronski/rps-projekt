7989a2099d42686a2d1448b496d2a6b940705f14
Krzysztof Niemaszyk <Krzysztof.Niemaszyk@sabre.com>
2016-06-11 23:38:36 +0200

    first hipothesis-exponential time of consecutive access times

--- skrypt.r
+++ skrypt.r
@@ -5,8 +5,8 @@ filename_full <- "C:\\Users\\Krzysiek\\Documents\\rps_projekt\\rps-projekt\\wycz
 file_full <- read.csv(filename_full)
 
 # tworzenie list dat i czasów logowań
-dates <- as.Date(as.POSIXct(file_full$authtime, origin="1970-01-01"))
-datetimes <- as.POSIXct(file_full$authtime, origin="1970-01-01")
+dates <- as.Date(as.POSIXct(file_full$date, origin="1970-01-01"))
+datetimes <- as.POSIXct(file_full$date, origin="1970-01-01")
 
 # agregcj dat logowań po datach (uniklnych dniach), miesiącach i latach
 plot(hist(dates, breaks="days"))
@@ -34,3 +34,27 @@ dates <- as.Date(as.POSIXct(wysokie_ip$authtime, origin="1970-01-01"))
 datetimes <- as.POSIXct(wysokie_ip$authtime, origin="1970-01-01")
 dates_weekdays <- format(datetimes, "%u")
 barplot(prop.table(table(dates_weekdays)))
+
+#czasy miedzy logowaniami do systemu
+shift <- function (x, shift) c(rep(NA,times=shift), x[1:(length(x)-shift)])
+differences <- file_full$date - shift(file_full$date, 1)
+differences <- differences[!is.na(differences)]
+differences <- differences[differences>=0]
+differences_Q2 <- quantile(differences, 0.5)
+differences_Q1<- quantile(differences, 0.25)
+differences_Q3 <- quantile(differences, 0.75)
+differences_IQR <- IQR(differences)
+differences_lbound <- differences_Q1-(differences_IQR*1.5)
+differences_ubound <- differences_Q3+(differences_IQR*1.5)
+differences_trimmed <- differences[(differences>differences_lbound) & (differences<differences_ubound)]
+plot (hist(differences_trimmed))
+require(MASS)
+fit <- fitdistr(differences_trimmed, "exponential")
+ks.test(differences_trimmed, "pexp", fit$estimate)
+#kontrola poprawności wykonywania testu
+control <- abs(rnorm(10000))
+fit2 <- fitdistr(control, "exponential")
+ks.test(control, "pexp", fit2$estimate)
+#proba testu chi kwadrat
+tmp <-rexp(length(differences_trimmed), rate = differences_trimmed_rate)
+chisq.test(differences_trimmed, p = tmp/sum(tmp))
